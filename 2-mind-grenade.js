2b3e6cbf86a7a2d2e4934b5f1eb372464917130d
Harsh Todwal <harshkhandelwal0098@gmail.com>
2024-11-28 11:03:39 +0530

    first commit

---
+++ 2-mind-grenade.js
@@ -0,0 +1,10 @@
+const n1 = 5
+const n2 = 10
+
+function add() {
+    console.log(n1+n2);
+}
+
+add()
+
+// require it in anyfile it will run auto without expo
---
+++ app.js
@@ -0,0 +1,11 @@
+const amount = 12
+
+let total = 0
+for (let i = 0; i < amount; i++) {
+    total += 5
+}
+
+console.log(total);
+
+// exports = {amount, total};
+// const name = require('./app.js');
